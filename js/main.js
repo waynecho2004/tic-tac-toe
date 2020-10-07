4ecc8ef3c3df201e039db28bdaa3144eb89a0593
Wayne Cho <waynecho2004@gmail.com>
2020-10-07 14:34:17 -0400

    basic game panel

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
+// Enable strict mode to eliminate some JavaScript silent errors 
+// by changing them to throw errors. And help JavaScript engines 
+// to perform optimizations to run our code faster.
+'use strict';
+
 console.log('It works!');
+
+const container = document.getElementsByClassName('container');
+console.log(document);
