375a773c3f0da2fe7fbc6dc915432ed9e0427959
Wayne Cho <waynecho2004@gmail.com>
2020-10-08 12:01:13 -0400

    game over

--- js/main.js
+++ js/main.js
@@ -29,39 +29,45 @@ let sequence = 0;
 let moves = [];
 let player = player1;
 let nextPlayer = player2;
+let gameOver = false;
 
 function moveHandler(e) {
     e.preventDefault();
    // console.log('Clicked');
-   const box = e.target.id;
-   console.log('boxSelected: ');
-   console.log(box);
+   const move = e.target.id;
+   console.log('moveSelected: ');
+   console.log(move);
 
-   // player can proceed the move only if the box is not taken
-   if(isNewMove(box)) {
+   // player can proceed the move only if the move is not taken
+   if (gameOver) {
+       alert('Game is over.  Please try another game');
+   }
+   else if(isNewMove(move)) {
         sequence += 1;
         console.log(player.name + '\'s move');
         // console.log(moves);
 
-        // update the moves made so far
-        moves.push(box);
-        player = getTurn(sequence, box);
-        player.move(box);
+        // update the game's moves made so far
+        moves.push(move);
+        player = getTurn(sequence, move);
+        player.move(move);
         
         // check for winning move
         if (sequence === draw) {
             console.log('The game is draw!');
+            gameOver = true;
             displayStatus('The game is draw!')
-        } else if(isPlayerWinning(player, box)) {
+        } else if(isPlayerWinning(player, move)) {
             console.log('Winner');
+            gameOver = true;
             displayStatus(`${player.name} wins!!!`)
         } else {
             console.log('game is not over yet');
             displayStatus(`${nextPlayer.name}\'s Turn`)
         }
-
-   } else {
-        alert('Wrong move, this box is already taken.  Please try again!');
+   } 
+   else {
+        alert('Wrong move, this move is already taken.  Please try again!');
    }
 }
 
@@ -71,26 +77,26 @@ function displayStatus(message) {
 
  /**
   * This determines current player and next player based on the play sequence.   
-  * And it marks the clicked box with appropriate symbol or text
+  * And it marks the clicked move with appropriate symbol or text
   * @param {*} num  sequence of the game
-  * @param {*} box  the box current player is clicked on 
+  * @param {*} move  the move current player is clicked on 
   * @returns 
   */
-function getTurn(num, box) {
-    console.log('step ' + num + ', move ' + box );
-    let selectedBox = document.querySelector(`#${box}`);
-    console.log(selectedBox);
+function getTurn(num, move) {
+    console.log('step ' + num + ', move ' + move );
+    let selectedmove = document.querySelector(`#${move}`);
+    console.log(selectedmove);
 
     if(num % 2 === 0) {
-        selectedBox.style.color = 'green';
-        selectedBox.innerHTML = 'O';
-        selectedBox.style.fontSize = 'xx-large';
+        selectedmove.style.color = 'green';
+        selectedmove.innerHTML = 'O';
+        selectedmove.style.fontSize = 'xx-large';
         nextPlayer = player1;
         return player2;
     } else {
-        selectedBox.style.color = 'blue';
-        selectedBox.innerHTML = 'X';
-        selectedBox.style.fontSize = 'xx-large';
+        selectedmove.style.color = 'blue';
+        selectedmove.innerHTML = 'X';
+        selectedmove.style.fontSize = 'xx-large';
         nextPlayer = player2;
         return player1;
     }
@@ -99,33 +105,33 @@ function getTurn(num, box) {
 function reset() {
     sequence = 1;
     moves = [];
-    // TODO: reset boxes
+    // TODO: reset movees
 }
 
-function isNewMove(box) {
-    return !moves.includes(box);
+function isNewMove(move) {
+    return !moves.includes(move);
 }
 
 container.addEventListener('click', moveHandler);
 
 /**
- * The determines if player is winning based on the box selected
+ * The determines if player is winning based on the move selected
  * @param {Player} player 
- * @param {*} box 
+ * @param {*} move 
  */
-function isPlayerWinning(player, box) {
+function isPlayerWinning(player, move) {
     // It takes at least 3 moves to win
     if (moves.length < 3) {
         return false;  
     }
 
-    // get the winning patterns for selected box
+    // get the winning patterns for selected move
     let winningPatterns = [];
     let isWinning = false;
-    winningPatterns = winningMap.get(box);
+    winningPatterns = winningMap.get(move);
     
     winningPatterns.forEach(winningPattern => {
-        console.log('winning pattern for ' + box + ': ' + winningPattern);
+        console.log('winning pattern for ' + move + ': ' + winningPattern);
         if (containWinningMatch(winningPattern, player.getPlayerMoves())) {
             isWinning = true;
         }
@@ -136,21 +142,21 @@ function isPlayerWinning(player, box) {
 
 /**
  * This determine if the moves player made match any winning pattern. 
- * If any two matching boxes are found, player wins
+ * If any two matching movees are found, player wins
  * @param {*} winningPattern 
  * @param {*} moves 
  */
 function containWinningMatch(winningPattern, moves) {
 
-    let matchingBoxesCount = 0;
-    winningPattern.forEach(box => {
-        if(moves.includes(box)) {
-            console.log(moves + ' includes ' + box);
-            matchingBoxesCount += 1;
+    let matchingmoveesCount = 0;
+    winningPattern.forEach(move => {
+        if(moves.includes(move)) {
+            console.log(moves + ' includes ' + move);
+            matchingmoveesCount += 1;
         }
     })
 
-    return matchingBoxesCount == 2;
+    return matchingmoveesCount == 2;
 }
 
 
