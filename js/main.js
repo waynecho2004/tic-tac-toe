7d7bd710cea3356647362ff480de3ad1ea934279
Wayne Cho <waynecho2004@gmail.com>
2020-10-09 20:53:23 -0400

    enhance ai logic

--- js/main.js
+++ js/main.js
@@ -3,8 +3,6 @@
 // to perform optimizations to run our code faster.
 'use strict';
 
-// console.log('It works!');
-
 // This map contains the matching pattern to win for each move
 const winningMap = new Map()
 winningMap.set('A1', [ ['A2', 'A3'], ['B2', 'C3'], ['B1', 'C1'] ]);
@@ -17,6 +15,7 @@ winningMap.set('C1', [ ['A1', 'B1'], ['A3', 'B2'], ['C2', 'C3'] ]);
 winningMap.set('C2', [ ['A2', 'B2'], ['C1', 'C3']]);
 winningMap.set('C3', [ ['A1', 'B2'], ['C1', 'C2'], ['A3', 'B3'] ]);
 
+const allMoves = ['A1', 'A2', 'A3', 'B1', 'B2', 'B3', 'C1', 'C2', 'C3'];
 const container = document.querySelector('.container');
 const playButton = document.querySelector('.button1');
 const boxes = document.querySelectorAll('.box');
@@ -24,6 +23,7 @@ const status = document.querySelector('h1');
 const player1Score = document.querySelector('#player1_score');
 const player2Score = document.querySelector('#player2_score');
 const tieScore = document.querySelector('#ties');
+const audio = new Audio("./audio/Button.wav");
 const tie = 9;
 
 // DECLARATION
@@ -63,9 +63,8 @@ function moveHandler(e) {
     const move = e.target.id;
     // console.log('moveSelected: ' + move);
 
-    // player can proceed the move only if the move is not taken
     if (gameOver) {
-        alert('Game over.  Please click Play to start another game');
+        alert('Game over.  Please click Play Again to start a new game');
     }
     else if(isNewMove(move)) {
  
@@ -75,10 +74,7 @@ function moveHandler(e) {
             // console.log('possible move: ' + automateMove());  // TODO: automate move
             container.style.pointerEvents = 'none';
             setTimeout(function(){ 
-                // makeMove(automateMove());  // THIS IS THE WORKING LOGIC
-
-                // makeMove(automateMoveLevel2(player2));  // Assuming computer is always player 2
-                makeMove(getMovePreventingPlayerFromWinning());
+                makeMove(automateMove());  
                 container.style.pointerEvents = 'auto';
             }, 1000);
         }
@@ -92,10 +88,8 @@ function moveHandler(e) {
 function makeMove(move) {
     playSound();
     sequence += 1;
-    
-    // console.log(moves);
-
-    // update the game's moves made so far
+ 
+    // update the game's moves
     moves.push(move);
     player = getTurn(sequence, move);
     player.move(move);
@@ -104,27 +98,24 @@ function makeMove(move) {
     // check for winning move
     if(isPlayerWinning(player, move)) {
         // console.log('********* Winner is ' + player.name + '**********');
-        
         wins.push(player.id);
         loses.push(nextPlayer.id);
         
         updateScoreBoard();
         displayStatus(`${player.name} wins!!!`, 'red')
     } else if (sequence === tie) {
-        // console.log('The game is tie!');
         ties += 1;
         updateScoreBoard();
         displayStatus('The game is tie!', 'green')
     } else {
-        // console.log('game is not over yet');
         displayStatus(`${nextPlayer.name}\'s Turn`)
     }
 }
 
 function playSound() {
     // console.log("Playing sound")
-    let audio = new Audio("./audio/Button.wav");
     audio.play();
+    // console.log("sound is played");
 }
 
 function updateScoreBoard() {
@@ -156,13 +147,11 @@ function getTurn(num, move) {
     console.log(selectedmove);
 
     if(num % 2 === 0) {
-        // selectedmove.style.color = 'green';
         let image = `<img src="./images/cat.jpeg" width='100px' height='100px'>`
         selectedmove.innerHTML = image
         nextPlayer = player1;
         return player2;
     } else {
-        // selectedmove.style.color = 'blue';
         let image = `<img src="./images/dog.jpeg" width='100px' height='100px'>`
         selectedmove.innerHTML = image;
         nextPlayer = player2;
@@ -171,94 +160,31 @@ function getTurn(num, move) {
 }
 
 /**
- * Returns the key of a random box that's not yet taken
+ * AI logic of choosing next available move
+ *  Step 1: find ai winning move
+ *  Step 2: find player's winning move
+ *  Step 3: use B2 if available
+ *  Step 4: use random move 
  */
 function automateMove() {
-    let allMoves = ['A1', 'A2', 'A3', 'B1', 'B2', 'B3', 'C1', 'C2', 'C3'];
-    let possibleMoves = allMoves.filter(mv => !moves.includes(mv));
-    // console.log('possible moves: ' + possibleMoves);
-    let index = getRandomIndex(possibleMoves.length);
-    // console.log('index: ' + index);
-    return possibleMoves[index];
-}
-
-/**
- * Assuming player always start first. Here's the winning prevention strategy to make game more difficult
- */
-function getMovePreventingPlayerFromWinning() {
     console.log('findPlayer1WinningMoveIfPossible');
-    // find all the available moves, 
-    let allMoves = ['A1', 'A2', 'A3', 'B1', 'B2', 'B3', 'C1', 'C2', 'C3'];
+    
     let possibleMoves = allMoves.filter(mv => !moves.includes(mv));
+    let aiWinningMove = possibleMoves.find(mv => isPlayerWinning(player2, mv));
+    let playerwinningMove = possibleMoves.find(mv => isPlayerWinning(player1, mv));
 
-    // and find the first that leads to winning move
-    for (let i=0; i< possibleMoves.length; i++) {
-        if(isPlayerWinning(player1, possibleMoves[i])) {
-            console.log('player\'s possible winning move ' + possibleMoves[i])
-            return possibleMoves[i];
-        }
-    }
-    // console.log('No winning move found');
-    if (possibleMoves.includes('B2')) { 
+    if (aiWinningMove) {
+        console.log('aiWinningMove move found ' + aiWinningMove)
+        return aiWinningMove;
+    } else if (playerwinningMove) {
+        console.log('winning move found ' + playerwinningMove)
+        return playerwinningMove;
+    } else if (possibleMoves.includes('B2')) { 
         return 'B2';
     } else {
-        let index = getRandomIndex(possibleMoves.length);
-        return possibleMoves[index];
-    }
-    
-    // console.log('index: ' + index);
-    
+        return possibleMoves[getRandomIndex(possibleMoves.length)];
+    } 
 }
-
-// /**
-//  * This is an enhancement, definitely not the best logic to beat the game. I called this level2
-//  * This finds the adjacent empty box that can leads to victory
-//  */
-// function automateMoveLevel2(ai) {
-//     // If this is first move, find one that empty adjacent box, record nextMoves
-//     let nextMoves = [];
-//     let move = null;
-//     if (ai.moves.length === 0) {
-//         move = automateMove();
-//         nextMoves = getNextMoves(move);
-//     } else {
-//         // from the nextMoves, find the one that's not taken
-//         nextMoves = ai.getPlayerNextMoves();
-//         // nextMoves = getNextMoves(move);
-
-//         if(nextMoves.length === 0) {
-//             move = automateMove();
-//             nextMoves = getNextMoves(move);
-//         } else {
-//             // get the moves not taken
-//             nextMoves = nextMoves.filter(move => !moves.includes(move));
-//             if (nextMoves.length === 0) {
-//                 move = automateMove();
-//                 nextMoves = getNextMoves(move);
-//             } else {
-//                 let size = nextMoves.length;
-//                 for(let i=0; i< size; i++) {
-//                     // check if any move leads to winning
-//                     if(isPlayerWinning(ai, nextMoves[i])) {
-//                         return nextMoves[i];
-//                     }
-//                 }
-                
-//                 let index = getRandomIndex(size)
-//                 move = nextMoves[index];
-//                 console.log('size: ' + size + ', index: ' + index + ', move ' + move);
-//                 // TODO: can following be enhanced to find one not empty
-//                 nextMoves = getNextMoves(move); 
-//             }
-//         }
-    
-
-
-//     }
-//     ai.setPlayerNextMoves(nextMoves);
-//     console.log('AI next moves ' + nextMoves);
-//     return move;
-// }
 
 /**
  * Get all the empty adjacent boxes as next moves
@@ -284,9 +210,9 @@ function isNewMove(move) {
  */
 function isPlayerWinning(player, move) {
     // It takes at least 5 moves to win
-    // if (moves.length < 5) {
-    //     return false;  
-    // }
+    if (moves.length < 4) {
+        return false;  
+    }
 
     // get the winning patterns for selected move
     let winningPatterns = [];
